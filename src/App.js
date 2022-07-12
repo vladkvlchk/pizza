b9aedfacd4fc94ab53d1604173e451a9e399b5af
Vladyslav <prostoandrei9@gmail.com>
2022-07-12 16:22:10 +0300

    add Cart-page
    +react-router-dom@6

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import PizzaBlock from './components/PizzaBlock';
-import Sort from './components/Sort';
-import React from 'react'
-import PBLoader from './components/PBLoader';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
-  const [items, setItems] = React.useState(['', '', '', '', '', '', '', '']);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(()=>{
-    fetch('https://62cc94cda080052930ada9ff.mockapi.io/all')
-    .then(res => res.json())
-    .then(arr => {
-      setItems(arr);
-      setIsLoading(false);
-    });
-  },[]);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj, index) => {
-              return ( isLoading ? <PBLoader /> :
-                <PizzaBlock
-                  key={`${obj.title.split(' ').join('').toUpperCase}_${index}`}
-                  {...obj}
-                />
-              );
-            })}
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Sort from '../components/Sort';
+import PBLoader from '../components/PBLoader';
+
+function Home() {
+  const [items, setItems] = React.useState(['', '', '', '', '', '', '', '']);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62cc94cda080052930ada9ff.mockapi.io/all')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {items.map((obj, index) => {
+            return isLoading ? (
+              <PBLoader key={index} />
+            ) : (
+              <PizzaBlock key={`${obj.title.split(' ').join('').toUpperCase}_${index}`} {...obj} />
+            );
+          })}
+        </div>
+      </div>
+  );
+}
+
+export default Home;
